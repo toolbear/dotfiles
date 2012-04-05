72082e9158fd726fb711ef986f7219d0fa2d83e7
Tim Taylor <tim@toolbear.io>
2012-04-05 14:03:23 -0400

    color-theme-solarized: switch from package to git submodule

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,11 @@
 (setq-default tab-width 2)
 (setq-default indent-tabs-mode nil)
 (setq inhibit-startup-message t)
+(setq-default scss-compile-at-save nil)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
+(load-theme 'solarized-dark t)
+(add-to-list 'auto-mode-alist '("COMMIT_EDITMSG$" . diff-mode))
 
 (require 'package)
 (add-to-list 'package-archives
@@ -16,7 +21,6 @@
  '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector (vector "#708183" "#c60007" "#728a05" "#a57705" "#2075c7" "#c61b6e" "#259185" "#042028"))
  '(current-language-environment "UTF-8")
- '(custom-enabled-themes (quote (solarized-dark)))
  '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
  '(make-backup-files nil)
  '(show-paren-mode t)
