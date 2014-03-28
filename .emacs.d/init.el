d46859a4d554a98dae448edbf002112bb4879a40
Tim Taylor <tim@toolbear.io>
2014-03-27 22:21:39 -0700

    emacs: remove Solarized for now
    Will restore it when theme installation is managed with boxen.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,9 +3,6 @@
 (setq-default indent-tabs-mode nil)
 (setq inhibit-startup-message t)
 (setq-default scss-compile-at-save nil)
-
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
-(load-theme 'solarized-dark t)
 (add-to-list 'auto-mode-alist '("COMMIT_EDITMSG$" . diff-mode))
 
 (custom-set-variables
@@ -13,10 +10,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector (vector "#708183" "#c60007" "#728a05" "#a57705" "#2075c7" "#c61b6e" "#259185" "#042028"))
  '(current-language-environment "UTF-8")
- '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
  '(make-backup-files nil)
  '(safe-local-variable-values (quote ((encoding . utf-8))))
  '(show-paren-mode t)
