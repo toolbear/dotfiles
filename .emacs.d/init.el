0c7be81072f28b8812127730da617f917f6a3f0c
Tim Taylor <tim@toolbear.io>
2014-03-27 22:30:06 -0700

    emacs: switch to Source Code Pro typeface
    ...and only set font when run as Emacs.app.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,12 @@
-(set-frame-font "Menlo-14")
+(when (display-graphic-p)
+  ;; FIXME style/adstyle "Light" is ignored if "Regular" is installed
+  (set-face-attribute 'default nil :font "Source Code Pro Light 14")
+  (set-fontset-font
+    (frame-parameter nil 'font)
+    'ipa
+    (font-spec :family "Menlo" :size 14)
+    nil
+    'prepend))
 (setq-default tab-width 2)
 (setq-default indent-tabs-mode nil)
 (setq inhibit-startup-message t)
