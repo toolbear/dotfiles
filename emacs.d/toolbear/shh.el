01ab17a1918085fd7c94ba61929efe11a6bc1556
Tim Taylor <tim@toolbear.io>
2015-02-26 22:49:59 -0800

    emacs: auto-revert modified files

--- emacs.d/toolbear/shh.el
+++ emacs.d/toolbear/shh.el
@@ -1,7 +1,8 @@
 (setq
   inhibit-startup-message t
   make-backup-files nil
-  create-lockfiles nil)
+  create-lockfiles nil
+  revert-without-query '(".*"))
 (when (not (display-graphic-p))
   (menu-bar-mode -1))
 (tool-bar-mode -1)
