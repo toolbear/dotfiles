768fbcd838c4c4b2b5e1ae7504310a60c2521a5a
Tim Taylor <tim@toolbear.io>
2015-02-26 22:30:31 -0800

    emacs: multi-term

---
+++ emacs.d/toolbear/terminal.el
@@ -0,0 +1,12 @@
+(require 'multi-term)
+(setq multi-term-program "zsh")
+(add-hook 'term-mode-hook
+  (lambda ()
+    (setq term-buffer-maximum-size 10000)
+    (setq show-trailing-whitespace nil)
+    ;;(autopair-mode -1)
+    ;;(colorful-points -1)
+    (add-to-list 'term-bind-key-alist '("C-M-[" . multi-term-prev))
+    (add-to-list 'term-bind-key-alist '("C-M-]" . multi-term-next))
+    (define-key term-raw-map (kbd "C-y") 'term-paste)
+    ))
