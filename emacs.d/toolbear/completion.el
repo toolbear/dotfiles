1ab4d695885f85454fbac9d3c2e6e48d2ef523f0
Tim Taylor <tim@toolbear.io>
2015-03-21 12:27:28 -0700

    emacs: completion with ido-mode and friends

---
+++ emacs.d/toolbear/completion.el
@@ -0,0 +1,19 @@
+;(require 'ido)
+;(ido-mode t)
+;(setq ido-enable-flex-matching t)
+
+(require 'ido)
+(ido-mode 1)
+
+(require 'flx-ido)
+(flx-ido-mode 1)
+
+(require 'ido-ubiquitous)
+(ido-ubiquitous-mode)
+
+(require 'flx-ido)
+(flx-ido-mode)
+
+(require 'ido-vertical-mode)
+(ido-vertical-mode 1)
+(setq ido-use-virtual-buffers t)
