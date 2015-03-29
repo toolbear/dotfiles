d5e67a6067819a1f6b45bff6279ea276a376c6a7
Tim Taylor <tim@toolbear.io>
2015-03-29 01:12:26 -0700

    emacs: fancy glyphs for truncated & wrapped lines

--- emacs.d/toolbear/no-line-wrap.el
+++ emacs.d/toolbear/no-line-wrap.el
@@ -1,1 +1,7 @@
 (set-default 'truncate-lines t)
+(set-display-table-slot standard-display-table
+                        'truncation
+                        (make-glyph-code ?⋮))
+(set-display-table-slot standard-display-table
+                        'wrap
+                        (make-glyph-code ?⤸))
