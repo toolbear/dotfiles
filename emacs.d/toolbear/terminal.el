ab910bbe6ad79fc3d1f0f32bd05a6d6add15bfee
Tim Taylor <tim@toolbear.io>
2015-03-30 18:11:11 -0700

    emacs: whitespace and cleanup

--- emacs.d/toolbear/terminal.el
+++ emacs.d/toolbear/terminal.el
@@ -3,16 +3,16 @@
   default-terminal-coding-system "utf-8-unix"
   multi-term-program "zsh")
 (global-unset-key (kbd "C-x m"))
-(define-key global-map (kbd "C-x m") 'multi-term)
+(global-set-key (kbd "C-x m") 'multi-term)
 (defun toolbear:entered-term-mode ()
-    (setq term-buffer-maximum-size 10000)
-    (define-key term-raw-map (kbd "C-y") 'term-paste)
-    (setq show-trailing-whitespace nil)
+  (setq term-buffer-maximum-size 10000)
+  (define-key term-raw-map (kbd "C-y") 'term-paste)
+  (setq show-trailing-whitespace nil)
 
-    (let ((disabled-modes '(autopair-mode
-                        colorful-points
-                        linum-mode
-                        hl-line-mode
-                        crosshairs-mode)))
-      (mapc (lambda (f) (when (fboundp f) (funcall f -1))) disabled-modes)))
+  (let ((disabled-modes '(autopair-mode
+                          colorful-points
+                          linum-mode
+                          hl-line-mode
+                          crosshairs-mode)))
+    (mapc (lambda (f) (when (fboundp f) (funcall f -1))) disabled-modes)))
 (add-hook 'term-mode-hook 'toolbear:entered-term-mode)
