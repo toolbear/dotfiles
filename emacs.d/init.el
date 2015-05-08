5e9955c75e90a20cebe3c01d80f9fafab3a074e6
Tim Taylor <tim@toolbear.io>
2015-05-07 18:54:37 -0700

    emacs: diminish mouse UI sooner

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,9 @@
+;; do these early so we never see them, not even for an instant
+;; otherwise they'd be in `toolbear/shh.el`
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
 (require 'cask)
 (cask-initialize)
 (boxed-wine-initialize)
--- emacs.d/toolbear/shh.el
+++ emacs.d/toolbear/shh.el
@@ -3,7 +3,4 @@
   make-backup-files nil
   create-lockfiles nil
   revert-without-query '(".*"))
-(when (not (display-graphic-p))
-  (menu-bar-mode -1))
-(tool-bar-mode -1)
 (fset 'yes-or-no-p 'y-or-n-p)
