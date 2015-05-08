59f773e7863575981947561de93e638d387ad8d2
Tim Taylor <tim@toolbear.io>
2015-05-07 21:46:11 -0700

    emacs: rocks multiple cursors

--- emacs.d/toolbear/cursor.el
+++ emacs.d/toolbear/cursor.el
@@ -3,3 +3,11 @@
   (set-default 'cursor-type '(hbar . 1))
   (set-default 'cursor-in-non-selected-windows 'hollow))
 (global-hl-line-mode +1)
+
+;; multiple-cursors
+;; http://emacsrocks.com/e13.html
+(require 'multiple-cursors)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "M-SPC") 'set-rectangular-region-anchor)
