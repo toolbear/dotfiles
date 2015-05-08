45609efd1b768df28539fe9c845893dba287601c
Tim Taylor <tim@toolbear.io>
2015-05-07 17:25:38 -0700

    emacs: tabbed TODOs; rename-tab alias for set-frame-name

--- emacs.d/toolbear/tabbed.el
+++ emacs.d/toolbear/tabbed.el
@@ -1,5 +1,18 @@
 ;; -*- lexical-binding: t -*-
 
+;; tabbed.el
+;; Emacs frames on a TTY are always full screen and map pretty cleanly to a mental
+;; model of tabs. This provides a (leaky) abstraction of frames as tabs.
+;;
+;; ## TODO
+;; * [ ] detect frame creation of the 1st frame from a new client (e.g. `emacsclient --tty`)
+;;   and rename it to 'tab#1'. Currently one must manually do `M-x rename-tab` and set it to
+;;   tab#1
+;; * [ ] when closing tab, renumber subsequent tabs; e.g. tab#(n) becomes tab#(n-1) where
+;;   n > number of the closed tab
+
+(defalias 'rename-tab 'set-frame-name)
+
 (defun tabs:new ()
   "Create a new frame named `tab#N` where `N` is the next available tab number."
   (interactive)
