8782f33dd788ea680289411d206a59a47b131758
Tim Taylor <tim@toolbear.io>
2016-04-05 08:53:52 -0700

    emacs: indent CoffeeScript by 2

--- emacs.d/toolbear/custom.el
+++ emacs.d/toolbear/custom.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ag-reuse-buffers t)
+ '(coffee-tab-width 2)
  '(compilation-auto-jump-to-first-error t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
