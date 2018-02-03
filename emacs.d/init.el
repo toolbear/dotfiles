20f1b5f40e49d7c6f7530694a8d9b36f5566ad75
Tim Taylor <tim@toolbear.io>
2018-02-03 12:26:03 -0800

    emacs: accumulated changes

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,5 +1,12 @@
 ;; do these early so we never see them, not even for an instant
 ;; otherwise they'd be in `toolbear/shh.el`
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
